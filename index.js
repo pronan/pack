5c99581316731cd71c0cff5813c10333194333ca
Your Name <you@example.com>
2016-08-19 11:48:46 +0800

    foo

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-require("./css/main.scss")
-foo=require("./js/a.js")
-$=require('./js/jquery-1.11.1.min.js')
+//import $ from 'jquery'
+import "./css/main.scss"
+//window.$=$
+
+function Foo() {
+    $('#nav').children().each(function (i, v){console.log(i, v)});
+}
+
+// ...
+
+module.exports = Foo;
--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,20 @@ module.exports = {
     entry: "./index.js",
     output: {
         path: '../wdksw/static/js',
-        filename: "bundle.js"
+        filename: "bundle.js", 
+        libraryTarget: "var",
+        library: "Foo", 
     },
+    externals: {
+        "jquery": "jQuery", 
+    }, 
     module: {
         loaders: [
             { test: /\.css$/, loader: "style!css" }, 
             { test: /\.scss$/, loaders: ["style", "css", "sass"]}, 
+            { test: /\.js$/, exclude: /(node_modules|bower_components)/, loader: 'babel',
+                query: { presets: ['es2015'], plugins: ['transform-runtime'], cacheDirectory: true}, 
+            }, 
         ], 
 
     }
