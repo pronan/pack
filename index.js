bb827387bf953af700c81872e7cfc69bbf80a9b4
t430i <you@example.com>
2016-08-22 01:16:52 +0800

    foo

--- index.js
+++ index.js
@@ -3,9 +3,21 @@
 // import hmac_sha1  from "crypto-js/hmac-sha1"
 // import utf16 from "crypto-js/enc-utf16"
 // import utf8 from "crypto-js/enc-utf8"
-require("./css/main.scss")
-var get_uptoken = require("./js/uploader")
 
-module.exports = {
-    get_uptoken:get_uptoken, 
-}
+require("./css/main.scss")
+$ = require("jquery")
+
+
+require.ensure(["./js/uploader"], function(require) {
+
+	if (window.location.pathname=='/login') {
+		var get_uptoken = require("./js/uploader")
+	}
+	//var get_uptoken = require("./js/uploader")
+
+});
+
+console.log(window.location.pathname)
+// module.exports = {
+//     get_uptoken:get_uptoken, 
+// }
--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,14 @@ module.exports = {
     entry: "./index.js",
     output: {
         path: '../wdksw/static/js',
+        publicPath: 'static/js/', 
         filename: "bundle.js", 
-        libraryTarget: "var",
-        library: "lib", 
+        // libraryTarget: "var",
+        // library: "lib", 
     },
-    externals: {
-        "jquery": "jQuery", 
-    }, 
+    // externals: {
+    //     "jquery": "jQuery", 
+    // }, 
     module: {
         loaders: [
             { test: /\.css$/, loader: "style!css" }, 
