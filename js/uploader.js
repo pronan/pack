4a96c7474bb01c45f9bcc3c90f93455db6a92abd
Your Name <you@example.com>
2016-08-23 11:47:04 +0800

    foo

--- js/uploader.js
+++ js/uploader.js
@@ -1,57 +1,38 @@
 var hmac_sha1 = require("crypto-js/hmac-sha1") 
 var enc_base64 = require("crypto-js/enc-base64")
-// import utf16 from "crypto-js/enc-utf16"
-// import utf8 from "crypto-js/enc-utf8"
-// import enc_base64 from "crypto-js/enc-base64"
-// import hmac_sha1  from "crypto-js/hmac-sha1"
-require("./qiniu") //QiniuJsSDK and Qiniu available
-// require("./plupload") // plupload available
+// var plupload = require("plupload")
+// var QiniuJsSDK = require("qiniu")
+require("./plupload")
+require("./qiniu")
 
-function utf16to8(str) {
-  var out, i, len, c;
-  out = "";
-  len = str.length;
-  for (i = 0; i < len; i++) {
-    c = str.charCodeAt(i);
-    if ((c >= 0x0001) && (c <= 0x007F)) {
-      out += str.charAt(i);
-    } else if (c > 0x07FF) {
-      out += String.fromCharCode(0xE0 | ((c >> 12) & 0x0F));
-      out += String.fromCharCode(0x80 | ((c >> 6) & 0x3F));
-      out += String.fromCharCode(0x80 | ((c >> 0) & 0x3F));
-    } else {
-      out += String.fromCharCode(0xC0 | ((c >> 6) & 0x1F));
-      out += String.fromCharCode(0x80 | ((c >> 0) & 0x3F));
-    }
-  }
-  return out;
+function safe64(str){
+  return str.replace(/\+/g, "-").replace(/\//g, "_");
 }
-
-function safe64(base64) {
-  base64 = base64.replace(/\+/g, "-");
-  base64 = base64.replace(/\//g, "_");
-  return base64;
+function base64encode(str) {
+  return (new Buffer(str)).toString('base64');
 };
 
-var get_uptoken = function(accessKey, secretKey, putPolicy) {
-  var encoded = new Buffer(JSON.stringify(putPolicy)).toString('base64') ;
-  var encoded_signed = hmac_sha1(encoded, secretKey).toString(enc_base64);
-  var upload_token = accessKey + ":" + safe64(encoded_signed) + ":" + encoded;
-  return upload_token;
+function get_uptoken(accessKey, secretKey, putPolicy) {
+  var encoded = safe64(base64encode(JSON.stringify(putPolicy)));
+  var encoded_signed = safe64(hmac_sha1(encoded, secretKey).toString(enc_base64));
+  return accessKey + ":" + encoded_signed + ":" + encoded;
 };
+
 
 function make_uploader(opts){
+  opts = opts || {}
   var deadline = Math.floor((new Date()).getTime()/1000)+ (opts.expires || 3600)
   var ak = opts.ak || '0maLJRi-p7lBucMKdgyLWGEdPpGlRQUTeW-bbL4l'
   var sk = opts.sk || 'vywueA_FOc7gWWYprL8t0aBSD0b24TKbILIVmYOP'
   var avatar_input_id = opts.avatar_id || '#id-avatar'
   var browse_button = opts.browse_button || 'id-avatar-qiniu'
+  var scope = opts.scope || 'files'
   return (new QiniuJsSDK()).uploader({
     runtimes: opts.runtimes || 'html5,flash,html4',      
     container: opts.container || 'qiniu-form',
     browse_button: browse_button, 
     domain: opts.domain || 'files.httper.cn', 
-    uptoken : get_uptoken(ak, sk, {scope:'files', deadline :deadline}), 
+    uptoken : get_uptoken(ak, sk, {scope:scope, deadline :deadline}), 
     max_file_size: opts.max_file_size || '4mb', 
     max_retries: opts.max_retries || 2,   
     get_new_uptoken: opts.get_new_uptoken || false,             // 设置上传文件的时候是否每次都重新获取新的uptoken
--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,20 @@ module.exports = {
     output: {
         path: '../wdksw/static/js',
         publicPath: '/static/js/', 
-        filename: "bundle.js", 
+        filename: "main.js", 
         // libraryTarget: "var",
         // library: "lib", 
     },
     // externals: {
-    //     "jquery": "jQuery", 
+    //     "qiniu": "QiniuJsSDK", 
+    //     "plupload":"plupload", 
     // }, 
     module: {
         loaders: [
+            //{ loader: 'babel', test: /\.js$/, exclude: /(node_modules|bower_components)/, query: { presets: ['es2015'], plugins: ['transform-runtime'], cacheDirectory: true}, }, 
             { test: /\.css$/, loader: "style!css" }, 
             { test: /\.scss$/, loaders: ["style", "css", "sass"]}, 
             { test: /\.coffee$/, loader: "coffee-loader" },
-            //{ loader: 'babel', test: /\.js$/, exclude: /(node_modules|bower_components)/, query: { presets: ['es2015'], plugins: ['transform-runtime'], cacheDirectory: true}, }, 
         ], 
 
     }
