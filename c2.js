e5f13ea59f5efc1f220f2a58df3f2a61024bc6e9
Your Name <you@example.com>
2016-08-24 11:50:51 +0800

    foo

---
+++ c2.js
@@ -0,0 +1,28 @@
+var webpack = require("webpack");
+
+module.exports = {
+    entry: "./index.js",
+    output: {
+        path: '../wdksw/static/js',
+        publicPath: '/static/js/', 
+        filename: "abbb.js", 
+        // libraryTarget: "var",
+        // library: "lib", 
+    },
+    // externals: {
+    //     "qiniu": "QiniuJsSDK", 
+    //     "plupload":"plupload", 
+    // }, 
+    plugins: [
+        new webpack.ProvidePlugin({$:'jquery', jQuery:'jquery'}), 
+    ], 
+    module: {
+        loaders: [
+            //{ loader: 'babel', test: /\.js$/, exclude: /(node_modules|bower_components)/, query: { presets: ['es2015'], plugins: ['transform-runtime'], cacheDirectory: true}, }, 
+            { test: /\.css$/, loader: "style!css" }, 
+            { test: /\.scss$/, loaders: ["style", "css", "sass"]}, 
+            { test: /\.coffee$/, loader: "coffee-loader" },
+        ], 
+
+    }
+};
--- js/uploader.js
+++ js/uploader.js
@@ -1,4 +1,4 @@
-require("./qiniu"); // bind `QiniuJsSDK`, `plupload` to window
+require("./qiniu"); // bind `QiniuJsSDK`, `plupload` to window.
 var hmac_sha1 = require("crypto-js/hmac-sha1") 
 var enc_base64 = require("crypto-js/enc-base64")
 
